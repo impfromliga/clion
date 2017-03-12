d57610a575fa64f9397c20c7044339da170c4825
impfromliga <impfromliga@users.noreply.github.com>
2017-03-12 12:02:27 +0300

    clion.js

--- clion.js
+++ clion.js
@@ -1,5 +1,8 @@
 "use strict";
 (function f(r,c){
+var require = {};
+if(r in require) return c();
+	
 function ModuleIframe(id,back){
 	console.log(r,"module onload");
 	var iframe = document.createElement('iframe');
@@ -15,60 +18,30 @@ function ModuleIframe(id,back){
 				back(val);
 			}
 		});
-		
 		var idoc = iwin.document;
 		var iscript = idoc.createElement('script');
-		iscript.onload = function(){
-			console.log(id+' script onload');
-		}
+		iscript.onload = function(){console.log(id+' script onload');}
 		iscript.src = id+'.js';
 		idoc.documentElement.appendChild(iscript);
 	}
 	document.documentElement.appendChild(iframe);
 }
 
-/*
-function sleep15(x){x = new XMLHttpRequest();x.open('GET','0.0.0.0',false);x.send(null)}
-
-console.log('iscript require('+val+')');
-		//console.log('this',this);
-		//console.log('compare require',require === window.top.require);
-		console.log('check "'+val+'" in require',val in require);
-		if(val in require){
-			console.log('typeof', typeof require[val] );
-			var t0 = window.performance.now();
-			while( typeof require[val] === 'undefined' ){
-				var t = window.performance.now();
-				sleep15()
-				console.log(window.performance.now()-t|0);
-				if(window.performance.now()-t0>1000)break;
-			}
-			console.log('iscript return require('+val+') await '+(window.performance.now()-t0)+'ms');
-			return require[val];
-		}
-*/
-
-
-var require = {};
-//console.log('require("'+r+'")');
-if(r in require) return c();
-
 var xhr	= new XMLHttpRequest();
 xhr.open('GET', r+'.js', true);
 xhr.onload = function (e) {//async parse
 	if (xhr.status === 200) {
-		var re = /require\(['"]?([^*|\:;"'<>?/]*)['"]?\)/g;
-		var str = xhr.responseText;
-		var txt;
 		var back = function(){
 			console.log(r,'deps:',back.total);
 			if(!~--back.total)
 				ModuleIframe(r,function(val){c&&c();f[r]=val});
 		}
 		back.total = 0;
+		var re = /require\(['"]?([^*|\:;"'<>?/]*)['"]?\)/g;
+		var str = xhr.responseText;
+		var txt;
 		while(txt = re.exec(str)){
 			console.log(r, 'require("'+txt[1]+'")')
-			//str=str.slice(0,txt.index)+'require["'+txt[1]+'"]'+str.slice(txt.index+txt[0].length-str.length);
 			back.total++;
 			f(txt[1],back)
 		}
